dac5b5b3cebc47b5f94e8a64de4ed275cbc3d9ea
julius-boettger <julius.btg@proton.me>
2024-04-09 13:42:21 +0200

    add basic routes

--- src/main/java/swsyspro/movies/MovieController.java
+++ src/main/java/swsyspro/movies/MovieController.java
@@ -1,14 +1,24 @@
 package swsyspro.movies;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MovieController
 {
-    @GetMapping("/movie")
-    public Movie getMovie ()
+    @PostMapping("/movies/{name}")
+    public long postMovie (@PathVariable String name)
     {
-        return new Movie(0, "test");
+        System.out.println("got movie: " + name);
+        return 0;
+    }
+
+    @GetMapping("/movies/{id}")
+    public Movie getMovie (@PathVariable int id)
+    {
+        System.out.println("requested id: " + id);
+        return new Movie(0, "uhhhh");
     }
 }
