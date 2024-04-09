3f26d9f72e6d1d8a5a76b0d68fed43caa1835e13
julius-boettger <julius.btg@proton.me>
2024-04-09 12:52:01 +0200

    set up spring boot

---
+++ src/main/java/swsyspro/movies/SwsysproMoviesApplication.java
@@ -0,0 +1,13 @@
+package swsyspro.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwsysproMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwsysproMoviesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/swsyspro/movies/SwsysproMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package swsyspro.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwsysproMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
