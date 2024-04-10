1eb9b82ce03a1d13dc6dd7dc95b1a75eb1150109
julius-boettger <julius.btg@proton.me>
2024-04-10 13:33:24 +0200

    add todo comments

--- src/main/java/swsyspro/movies/MovieController.java
+++ src/main/java/swsyspro/movies/MovieController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
 
+// TODO set up docker compose
+
 @RestController
 public class MovieController
 {
@@ -17,6 +19,7 @@ public class MovieController
     private final AtomicLong next_id = new AtomicLong();
 
 
+    // TODO accept json movie (without id?) as post request body instead of path variable
     @PostMapping("/movies/{name}")
     public long postMovie (@PathVariable String name)
     {
@@ -24,11 +27,15 @@ public class MovieController
         this.movies.put(id, new Movie(id, name));
         System.out.printf("got movie \"%s\", returning id %d\n", name, id);
         return id;
+        // TODO return whole movie, not just id
+        // TODO return code 201
     }
 
+    // TODO refactor to /movies/id/{id}
     @GetMapping("/movies/{id}")
     public Movie getMovie (@PathVariable long id)
     {
+        // TODO return error message with code 404 if id doesn't exist
         Movie movie = movies.get(id);
         System.out.printf("requested id %d, returning %s\n", id, movie);
         return movie;
@@ -40,4 +47,7 @@ public class MovieController
         System.out.println("requested all movies");
         return this.movies.values();
     }
+
+    // TODO GET /movies/name/{name} endpoint
+    // TODO DELETE /movies/id/{id} endpoint
 }
